07270db470906b87013a9ae931a40ecec22ffb1e
gadzhi <gadzhi7.7.7@mail.ru>
2019-06-26 14:11:02 +0300

    added test.json write not a lot of code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,51 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+  const final = document.querySelector('.final'),
+        startBlock = document.querySelector('.start'),
+        questionBlock = document.querySelector('.question'),
+        questionOptions = document.querySelector('.question_options'),
+        questionAnswer = document.querySelector('.question_answer'),
+        currentQuestionText = document.querySelector('.question_count__current'),
+        totalQuestionText = document.querySelector('.question_count__total'),
+        questionText = document.querySelector('.question_text');
+
+
+  const startBtn = document.querySelector('.start_button'),
+        nextBtn = document.querySelector('.question_next'),
+        againBtn = document.querySelector('.again_button');
+
+
+  let tests,
+      testTotalCount = 0,
+      rightAnswers = 0,
+      currentQuestion = 0;
+
+
+
+  //получаю тесты
+  let promise = fetch('test.json')
+  .then((response) => response.status === 200 ? response.json() : null)
+  .then((json) =>  {
+
+    json !== null ? tests = json : tests = null
+    show(startBtn);
+  })
+
+startBtn.addEventListener('click', function () {
+  
+  console.log(tests[0]);
 })
+
+
+
+})
+
+
+
+function show(el) {
+  el.classList.remove('hidden')
+}
+
+function hide(el) {
+  el.classList.add('hidden')
+}
